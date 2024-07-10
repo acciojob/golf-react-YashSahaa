06b1be22fed0f75635a2e9240572970973f760e4
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-07-10 12:56:21 +0530

    submission 2

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,9 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi: 0,
-            ballPosition: { left: "0px" }
+            posix: 0,
+            posiy: 0,
+            ballPosition: { left: "0px" ,top:"0px" }
         };
         this.renderChoice = this.renderBallOrButton.bind(this);
         this.buttonClickHandler = this.buttonClickHandler.bind(this);
@@ -21,16 +22,28 @@ class App extends Component {
     }
 
     handleKeyDown(event) {
-        if (event.key === 'ArrowRight' || event.keyCode === 39) {
+        if ( event.keyCode === 39) {
             this.setState((prevState) => ({
-                posi: prevState.posi + 5,
-                ballPosition: { left: `${prevState.posi + 5}px` }
+                posix: prevState.posix + 5,
+                ballPosition: { left: `${prevState.posix + 5}px`, top: `${prevState.posiy}px` }
             }));
         }
-        if (event.key === 'ArrowLeft' || event.keyCode === 37) {
+        else if (event.keyCode === 37) {
             this.setState((prevState) => ({
-                posi: prevState.posi - 5,
-                ballPosition: { left: `${prevState.posi - 5}px` }
+                posix: prevState.posix - 5,
+                ballPosition: { left: `${prevState.posix - 5}px`, top: `${prevState.posiy}px` }
+            }));
+        }
+        else if (event.keyCode === 38) {
+            this.setState((prevState) => ({
+                posiy: prevState.posiy - 5,
+                ballPosition: { left: `${prevState.posix}px`, top: `${prevState.posiy-5}px` }
+            }));
+        }
+        else if (event.keyCode === 40) {
+            this.setState((prevState) => ({
+                posiy: prevState.posiy + 5,
+                ballPosition: { left: `${prevState.posix}px`, top: `${prevState.posiy+5}px` }
             }));
         }
     }
